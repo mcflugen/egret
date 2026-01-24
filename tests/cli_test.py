1969cea2fd3319f697c552b4ef80bc9ea8a5c3ad
mcflugen <mcflugen@gmail.com>
2026-01-23 18:34:47 -0700

    add unit test for print-types option

--- tests/cli_test.py
+++ tests/cli_test.py
@@ -1,4 +1,5 @@
 import pytest
+from grist import ALL_TAGS
 from grist import main
 
 
@@ -19,3 +20,14 @@ def test_walk(capsys, tmpdir):
         main(["--walk", "foobar", "--types-or=text"])
         out = capsys.readouterr().out.strip()
         assert out.partition(":") == ("baz.txt", ":", "foobar")
+
+
+def test_print_types(capsys):
+    main(["--print-types"])
+    lines = capsys.readouterr().out.splitlines()
+    assert {line.strip() for line in lines} == ALL_TAGS
+
+
+def test_bad_tag(capsys):
+    assert main(["--walk", "foobar", "--types=foobar"]) == 1
+    assert capsys.readouterr().err.startswith("unrecognized tag")
